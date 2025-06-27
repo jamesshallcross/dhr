53f43fdea9c70a474d285334d360053254d6eea5
James Shallcross <james+github@bozboz.co.uk>
2025-06-27 12:15:52 +0100

    add -ns argument to use specific nameserver for lookups

--- dhr.sh
+++ dhr.sh
@@ -3,63 +3,94 @@
 # Domain Health Reporter (dhr)
 # Script to check domain info, DNS records, hosting, SSL and redirects
 
+# Parse command line arguments
+dns_server=""
+domain=""
+
+# Loop through all command line arguments until none left
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -ns)
+            # If current argument is -ns, then next argument ($2) is the DNS server
+            dns_server="@$2"
+            # Skip both the -ns flag and the DNS server value (shift by 2 positions)
+            shift 2
+            ;;
+        *)
+            # Any other argument is treated as the domain name
+            domain="$1"
+            # Move to next argument (shift by 1 position)
+            shift
+            ;;
+    esac
+done
+
 # Check if domain argument provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $(basename $0) <domain>"
+if [ -z "$domain" ]; then
+    echo "Usage: $(basename $0) [-ns dns_server] <domain>"
     echo "Example: $(basename $0) example.com"
-    echo "Example: $(basename $0) https://www.example.com/path"
+    echo "Example: $(basename $0) -ns 1.1.1.1 example.com"
+    echo "Example: $(basename $0) -ns 8.8.8.8 https://www.example.com/path"
     exit 1
 fi
 
 echo
-dns_server=`dig | grep SERVER | cut -d " " -f 2,3 | cut -d "#" -f 1`
-printf "Lookups using DNS $dns_server"
+# Check if dns_server variable is empty (user didn't specify -ns option)
+if [ -z "$dns_server" ]; then
+    # No custom DNS server specified - get system default DNS info
+    dns_info=`dig | grep SERVER | cut -d " " -f 2,3 | cut -d "#" -f 1`
+    printf "Lookups using DNS $dns_info"
+else
+    # Custom DNS server was specified - display it (strip @ symbol for clean output)
+    # ${dns_server#@} removes the "@" prefix we added earlier
+    printf "Lookups using DNS ${dns_server#@}"
+fi
 echo
 
 # Extract clean domain from various input formats
-domain=`echo $1 | awk -F "//" '{print $NF}' | awk -F "www." '{print $NF}' | awk -F "/" '{print $1}'`
+domain=`echo $domain | awk -F "//" '{print $NF}' | awk -F "www." '{print $NF}' | awk -F "/" '{print $1}'`
 echo -e "Domain : $(tput setaf 1)$domain$(tput sgr0) - finding IP & Ownership..."
 echo
 
 # Get IP and organization info for root domain
-NON_WWW_IP=`dig +short $domain | head -n 1 | xargs | sed -e 's/ /, /g'`
-NON_WWW_IP_ORG=`dig +short $domain | head -n 1 | xargs whois | grep 'OrgName\|org-name\|descr' | sort -r | head -n 1 | awk '{print $2,$3,$4,$5}'`
+NON_WWW_IP=`dig $dns_server +short $domain | head -n 1 | xargs | sed -e 's/ /, /g'`
+NON_WWW_IP_ORG=`dig $dns_server +short $domain | head -n 1 | xargs whois | grep 'OrgName\|org-name\|descr' | sort -r | head -n 1 | awk '{print $2,$3,$4,$5}'`
 echo -e "IP for root\t: ${NON_WWW_IP}\t: ${NON_WWW_IP_ORG}"
 
 # Get IP and organization info for www subdomain
-WWW_IP=`dig +short www.$domain | head -n 1 | xargs | sed -e 's/ /, /g'`
-WWW_IP_ORG=`dig +short www.$domain | head -n 1 | xargs whois | grep 'OrgName\|org-name\|descr' | sort -r | head -n 1 | awk '{print $2,$3,$4,$5}'`
+WWW_IP=`dig $dns_server +short www.$domain | head -n 1 | xargs | sed -e 's/ /, /g'`
+WWW_IP_ORG=`dig $dns_server +short www.$domain | head -n 1 | xargs whois | grep 'OrgName\|org-name\|descr' | sort -r | head -n 1 | awk '{print $2,$3,$4,$5}'`
 echo -e "IP for www\t: ${WWW_IP}\t: ${WWW_IP_ORG}"
 echo
 
 # A records for root domain
-dig $domain | grep IN | grep -v ";" | grep -v NS | sort -k 5n,5
+dig $dns_server $domain | grep IN | grep -v ";" | grep -v NS | sort -k 5n,5
 echo
 
 # A records for www subdomain
-dig www.$domain | grep IN | grep -v ";" | grep -v NS
+dig $dns_server www.$domain | grep IN | grep -v ";" | grep -v NS
 echo
 
 # MX records
-dig MX $domain | grep IN | grep -v ";" | grep MX | sort -k 5n,5
-dig MX $domain | grep IN | grep -v ";" | grep -v NS | sort -k 5n,5 | cuts -1 | xargs dig | grep IN | grep -v ";" | grep -v NS | grep -v SOA
+dig $dns_server MX $domain | grep IN | grep -v ";" | grep MX | sort -k 5n,5
+dig $dns_server MX $domain | grep IN | grep -v ";" | grep -v NS | sort -k 5n,5 | cuts -1 | xargs dig $dns_server | grep IN | grep -v ";" | grep -v NS | grep -v SOA
 echo
 
 # NS records
-dig NS $domain | grep IN | grep -v ";" | sort -k 5n,5
+dig $dns_server NS $domain | grep IN | grep -v ";" | sort -k 5n,5
 echo
 
 # TXT records
-dig TXT $domain | grep IN | grep -v ";" | grep -v NS | grep -v SOA
+dig $dns_server TXT $domain | grep IN | grep -v ";" | grep -v NS | grep -v SOA
 echo
 
 # DMARC record
-dig TXT _dmarc.$domain | grep IN | grep \"v
+dig $dns_server TXT _dmarc.$domain | grep IN | grep \"v
 echo
 
 # DKIM records (common selectors)
-dig TXT bozmail._domainkey.$domain | grep IN | grep \"v
-dig TXT boz._domainkey.$domain | grep IN | grep \"v
+dig $dns_server TXT bozmail._domainkey.$domain | grep IN | grep \"v
+dig $dns_server TXT boz._domainkey.$domain | grep IN | grep \"v
 echo
 
 # Registrar information
